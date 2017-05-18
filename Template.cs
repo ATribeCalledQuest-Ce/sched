46c358a6eb28a83a033a16b626ca38c6161f27d1
ATribeCalledQuest-Ce <slothman5339@gmail.com>
2017-05-18 13:50:38 -0700

    second commit

---
+++ Template.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+//Change namespace to something different
+namespace Template 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+		}
+	}
+}
