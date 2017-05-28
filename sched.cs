ceb7f7c68c90c83210926f7b8bb65bb123d1c87a
ATribeCalledQuest-Ce <slothman5339@gmail.com>
2017-05-28 02:26:54 -0700

    added a lot of comments to the code, hopefully clearer now.

--- sched.cs
+++ sched.cs
@@ -7,16 +7,19 @@ namespace Sched
 {
     public class Program
     {
-        static Char Number2String(int number, bool isCaps)
+        //Converts a number to a string so that the lists look nicer.
+		static Char Number2String(int number, bool isCaps)
         {
             Char c = (Char)((isCaps ? 65 : 97) + (number - 1));
             return c;
         }
-        static void wProf(string UserProfName, string path, string storagePath)
+        //Writes the profile name and path for that profile in the profiles folder
+		static void wProf(string UserProfName, string path, string storagePath)
         {
             string profInfo = String.Format("{0}, {1}", UserProfName, path);
             File.AppendAllText(storagePath, profInfo + Environment.NewLine);
         }
+		//Prompts the user for their periods and creates the profile file.
 		static string CreateProf(string userprof, string userpath)
         {
             int i = 0;
@@ -29,7 +32,8 @@ namespace Sched
                 Periods[i] = Console.ReadLine();
                 i++;
             }
-            if (userpath == "D" || userpath == "d")
+            //Writes the periods in the profile file, based on the directory given.
+			if (userpath == "D" || userpath == "d")
             {
                 string path = String.Format(@"Schedules\{0}.txt", userprof);
                 File.WriteAllLines(path, Periods);
@@ -42,18 +46,21 @@ namespace Sched
                 return path;
             }
         }
+		//Serves as the main UI for the program, final output is in here.
         public static void Main(string[] args)
         {
+			//Setting up some variables.
+			TimeSpan time = DateTime.Now.TimeOfDay;
+            string today = DateTime.Today.DayOfWeek.ToString();
             //Writes the introduction and asks the user what they want to do.
             Console.WriteLine("[redacted for github]. \nThis program is not done \n{0}, {1}\nPress N to create a new schedule.  Press U to use an existing schedule", DateTime.Today.DayOfWeek, DateTime.Now.TimeOfDay);
             string inp = Console.ReadLine();
-            TimeSpan time = DateTime.Now.TimeOfDay;
-            string today = DateTime.Today.DayOfWeek.ToString();
             if (inp == "N" || inp == "n")
             {
                 bool space = true;
                 string UserProfName = null;
-                while (space == true)
+                //Makes sure there is no spaces in the schedule name.
+				while (space == true)
                 {
                     Console.WriteLine("What do you want your schedule to be called?");
                     UserProfName = Console.ReadLine();
@@ -66,61 +73,50 @@ namespace Sched
                         Console.WriteLine("Spaces are not allowed in profile names, all other characters are allowed.");
                     }
                 }
-                Console.WriteLine("Where do you want your sched to be stored? \nIf you want to store it in the default location (sched\\Profiles\\YourProfileName.txt) press D. \nDefault is recommended, as it will work without further configuration of files etc");
+                //Asks where the user wants to store their schedule and recommends default, then writes it by calling wProf.
+				Console.WriteLine("Where do you want your schedule to be stored? \nIf you want to store it in the default location (sched\\Profiles\\YourProfileName.txt) press D. \nDefault is recommended, as it will work without further configuration of files etc");
                 string UserPath = Console.ReadLine();
                 string path = CreateProf(UserProfName, UserPath);
                 wProf(UserProfName, path, @"Profiles\Profs.txt");
-                //path is directory of periods
-                //Userpath is what the user defined as their name
+                //path is the full directory of periods
+                //UserPath is the containing folder or "D" for defualt.
             }
             //Start of Period Finder/Reader
             if (inp == "U" || inp == "u")
             {
-                Console.WriteLine("Press P to find your schedule by path, press U to find it based on a user profile entered earlier.");
-                string choice = Console.ReadLine();
-                if (choice == "u" || choice == "U")
+				Console.Write("Type the name of the profile you wish to use: ");
+                var keyword = Console.ReadLine();
+                using (var sr = new StreamReader(@"Profiles\Profs.txt"))
                 {
-                    Console.Write("Type the name of the profile you wish to use: ");
-                    var keyword = Console.ReadLine();
-                    using (var sr = new StreamReader(@"Profiles\Profs.txt"))
+                    while (!sr.EndOfStream)
                     {
-                        while (!sr.EndOfStream)
+                        var line = sr.ReadLine();
+                        if (String.IsNullOrEmpty(line)) continue;
+                        if (line.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0)
                         {
-                            var line = sr.ReadLine();
-                            if (String.IsNullOrEmpty(line)) continue;
-                            if (line.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0)
-                            {
-                                string[] splitString = line.Split(' ');
-                                string[] userPeriods = (ReadPath(splitString[1]));
-								int finalPeriod = PeriodTest(today, time);
-								int finalPeriodPlus = finalPeriod + 1;
-                                //
-								//
-								//POINT OF ENTIRE PROGRAM
-								//
-								//
-								if (finalPeriod == 100) Console.WriteLine("It is a weekend, or something has gone wrong.");
-								if (finalPeriod == 98 && today != "Thursday") Console.WriteLine("It is lunch, your next period is {0}", userPeriods[4]);
-								if (finalPeriod == 98 && today == "Thursday") Console.WriteLine("It is lunch and your next period is {0}", userPeriods[5]);
-								if (finalPeriod > 9) Console.WriteLine("Your current period is {0}, your next period is {1}", userPeriods[finalPeriod], userPeriods[finalPeriodPlus]);
-                            }
+                            //splits the line that has the desired profile on it into 2 parts of a string array, the name of the profile and the directory of the profile.
+							string[] splitString = line.Split(' ');
+                            //Calles the ReadPath function which uses the directory from splitSring[1] to put each period from the directory (splitSring[0]) into an array (userPeriods[])
+							string[] userPeriods = (ReadPath(splitString[1]));
+							int finalPeriod = PeriodTest(today, time);
+							int finalPeriodPlus = finalPeriod + 1;
+                            //
+							//
+							//POINT OF ENTIRE PROGRAM
+							//
+							//
+							if (finalPeriod == 100) Console.WriteLine("It is a weekend, or something has gone wrong.");
+							if (finalPeriod == 98 && today != "Thursday") Console.WriteLine("It is lunch, your next period is {0}", userPeriods[4]);
+							if (finalPeriod == 98 && today == "Thursday") Console.WriteLine("It is lunch and your next period is {0}", userPeriods[5]);
+							if (finalPeriod == 97) Console.WriteLine("You should be in seminar and your next class is {0}", userPeriods[2]);
+							if (finalPeriod < 9) Console.WriteLine("Your current period is {0}, your next period is {1}", userPeriods[finalPeriod], userPeriods[finalPeriodPlus]);
                         }
                     }
-                }
-                if (choice == "p" || choice == "P")
-                    {
-                        Console.WriteLine("What is the path of the schedule you would like to use?");
-                        string currentSched = Console.ReadLine();
-                        string[] UserPeriods = (ReadPath(currentSched));
-                        Console.WriteLine("You should be in {0}", PeriodTest(today, time));
-                        foreach (string period in UserPeriods)
-                        {
-                            Console.WriteLine(period);
-                        }
-                    }
+                }  
             }
             Console.ReadKey();
         }
+		//uses the directory from splitSring[1](Ppath) to put each period from the directory (splitSring[0]) into an array (userPeriods[]), returned here as lines[]
         static string[] ReadPath(string Ppath)
         {
             string[] lines = new string[7];
@@ -132,6 +128,7 @@ namespace Sched
             }
             return lines;
         }
+		//Main logic of the program, checks what day it is and uses the current time to choose the current period.
         public static int PeriodTest(string day, TimeSpan time)
         {
             //TimeSpan Test1 = new TimeSpan(0, 8, 0, 0);
@@ -147,6 +144,7 @@ namespace Sched
 				TimeSpan Ce = new TimeSpan(0, 10, 45, 0);
 				TimeSpan Ds = new TimeSpan(0, 10, 50, 0);
 				TimeSpan De = new TimeSpan(0, 11, 40, 0);
+				//lunch
 				TimeSpan Ls = new TimeSpan(0, 11, 40, 0);
 				TimeSpan Le = new TimeSpan(0, 12, 55, 0);
 				TimeSpan Es = new TimeSpan(0, 12, 55, 0);
