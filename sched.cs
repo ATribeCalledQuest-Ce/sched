de59502ec85b5d51f40c7a4d092e412295794c3e
ATribeCalledQuest-Ce <slothman5339@gmail.com>
2017-06-17 00:14:16 -0700

    made the code more concise

--- sched.cs
+++ sched.cs
@@ -8,24 +8,21 @@ namespace Sched
     public class Program
     {
         //Converts a number to a string so that the lists look nicer.
-		static Char Number2String(int number, bool isCaps)
-        {
+		static Char Number2String(int number, bool isCaps){
             Char c = (Char)((isCaps ? 65 : 97) + (number - 1));
             return c;
         }
         //Writes the profile name and path for that profile in the profiles folder
-		static void wProf(string UserProfName, string path, string storagePath)
-        {
+		static void wProf(string UserProfName, string path, string storagePath){
             string profInfo = String.Format("{0}, {1}", UserProfName, path);
             File.AppendAllText(storagePath, profInfo + Environment.NewLine);
         }
 		//Prompts the user for their periods and creates the profile file.
 		static string CreateProf(string userprof, string userpath)
-        {
+		{
             int i = 0;
             string[] Periods = new string[7];
-            while (i < 7)
-            {
+            while (i < 7){
                 int j = i + 1;
                 char per = Number2String(j, true);
                 Console.WriteLine("Please enter your {0} period class.", per);
@@ -33,14 +30,12 @@ namespace Sched
                 i++;
             }
             //Writes the periods in the profile file, based on the directory given.
-			if (userpath == "D" || userpath == "d")
-            {
+			if (userpath == "D" || userpath == "d"){
                 string path = String.Format(@"Schedules\{0}.txt", userprof);
                 File.WriteAllLines(path, Periods);
                 return path;
             }
-            else
-            {
+            else{
                 string path = String.Format(@userpath, userprof, ".txt");
                 File.WriteAllLines(path, Periods);
                 return path;
@@ -48,30 +43,22 @@ namespace Sched
         }
 		//Serves as the main UI for the program, final output is in here.
         public static void Main(string[] args)
-        {
+		{
 			//Setting up some variables.
 			TimeSpan time = DateTime.Now.TimeOfDay;
             string today = DateTime.Today.DayOfWeek.ToString();
             //Writes the introduction and asks the user what they want to do.
             Console.WriteLine("[redacted for github]. \nThis program is not done \n{0}, {1}\nPress N to create a new schedule.  Press U to use an existing schedule", DateTime.Today.DayOfWeek, DateTime.Now.TimeOfDay);
             string inp = Console.ReadLine();
-            if (inp == "N" || inp == "n")
-            {
+            if (inp == "N" || inp == "n"){
                 bool space = true;
                 string UserProfName = null;
                 //Makes sure there is no spaces in the schedule name.
-				while (space == true)
-                {
+				while (space == true){
                     Console.WriteLine("What do you want your schedule to be called?");
                     UserProfName = Console.ReadLine();
-                    if(!UserProfName.Contains(' '))
-                    {
-                        space = false;
-                    }
-                    if(UserProfName.Contains(' '))
-                    {
-                        Console.WriteLine("Spaces are not allowed in profile names, all other characters are allowed.");
-                    }
+                    if(!UserProfName.Contains(' '))space = false;
+                    if(UserProfName.Contains(' ')) Console.WriteLine("Spaces are not allowed in profile names, all other characters are allowed.");
                 }
                 //Asks where the user wants to store their schedule and recommends default, then writes it by calling wProf.
 				Console.WriteLine("Where do you want your schedule to be stored? \nIf you want to store it in the default location (sched\\Profiles\\YourProfileName.txt) press D. \nDefault is recommended, as it will work without further configuration of files etc");
@@ -82,18 +69,14 @@ namespace Sched
                 //UserPath is the containing folder or "D" for defualt.
             }
             //Start of Period Finder/Reader
-            if (inp == "U" || inp == "u")
-            {
+            if (inp == "U" || inp == "u"){
 				Console.Write("Type the name of the profile you wish to use: ");
                 var keyword = Console.ReadLine();
-                using (var sr = new StreamReader(@"Profiles\Profs.txt"))
-                {
-                    while (!sr.EndOfStream)
-                    {
+                using (var sr = new StreamReader(@"Profiles\Profs.txt")){
+                    while (!sr.EndOfStream){
                         var line = sr.ReadLine();
                         if (String.IsNullOrEmpty(line)) continue;
-                        if (line.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0)
-                        {
+                        if (line.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0){
                             //splits the line that has the desired profile on it into 2 parts of a string array, the name of the profile and the directory of the profile.
 							string[] splitString = line.Split(' ');
                             //Calles the ReadPath function which uses the directory from splitSring[1] to put each period from the directory (splitSring[0]) into an array (userPeriods[])
@@ -121,8 +104,7 @@ namespace Sched
         {
             string[] lines = new string[7];
             int i = 0;
-            foreach (string line in File.ReadLines(Ppath))
-            {
+            foreach (string line in File.ReadLines(Ppath)){
                 lines[i] = line;
                 i++;
             }
@@ -134,8 +116,7 @@ namespace Sched
             //TimeSpan Test1 = new TimeSpan(0, 8, 0, 0);
             //TimeSpan Test2 = new TimeSpan(0, 8, 0, 0);
             int CurrentPeriod = 0;
-			if (day == "Monday" || day == "Friday")
-            {	
+			if (day == "Monday" || day == "Friday"){	
 				TimeSpan As = new TimeSpan(0, 8, 0, 0);
 				TimeSpan Ae = new TimeSpan(0, 8, 50, 0);
 				TimeSpan Bs = new TimeSpan(0, 8, 55, 0);
@@ -162,8 +143,7 @@ namespace Sched
 				if (time > Gs && time < Ge)	CurrentPeriod = 6;
 				if (time > Ls && time < Le) CurrentPeriod =  98;
             }
-            if (day == "Tuesday")
-            {
+            if (day == "Tuesday"){
 				TimeSpan As = new TimeSpan(0, 8, 0, 0);
 				TimeSpan Ae = new TimeSpan(0, 8, 45, 0);
 				TimeSpan Bs = new TimeSpan(0, 8, 50, 0);
@@ -194,8 +174,7 @@ namespace Sched
 				if (time > Gs && time < Ge)	CurrentPeriod = 6;
 				if (time > Ls && time < Le) CurrentPeriod = 98;
             }
-            if (day == "Wendsday")
-			{
+            if (day == "Wendsday"){
 				TimeSpan As = new TimeSpan(0, 8, 0, 0);
 				TimeSpan Ae = new TimeSpan(0, 9, 40, 0);
 				TimeSpan Cs = new TimeSpan(0, 9, 55, 0);
@@ -214,8 +193,7 @@ namespace Sched
 				if (time > Es && time < Ee) CurrentPeriod = 4;
 				if (time > Ls && time < Le) CurrentPeriod = 98;
             }
-            if (day == "Thursday")
-            {
+            if (day == "Thursday"){
 				TimeSpan Bs = new TimeSpan(0, 8, 0, 0);
 				TimeSpan Be = new TimeSpan(0, 9, 40, 0);
 				TimeSpan Ds = new TimeSpan(0, 10, 50, 0);
@@ -233,10 +211,7 @@ namespace Sched
 				if (time > Gs && time < Ge) CurrentPeriod = 6;
 				if (time > Ls && time < Le) CurrentPeriod =  98;
 			}
-            if (day == "Saturday" || day == "Sunday")
-            {
-                CurrentPeriod = 100;
-            }
+            if (day == "Saturday" || day == "Sunday") CurrentPeriod = 100;
 			//Final Return
             return CurrentPeriod;
         }
